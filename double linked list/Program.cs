3580f208436cdb5897a782a9fa3c3291fa797922
Siti Marfungah Dwi Lestari <siti.marfungah.ft21@mail.umy.ac.id>
2022-11-18 16:52:03 +0700

    add class Node

--- double linked list/Program.cs
+++ double linked list/Program.cs
@@ -6,10 +6,17 @@ namespace double_linked_list
     {
         class Node
         {
-            //Node class 
+            //Node class represent the node of doubly linked list
+            //It consist of the information part and links to
+            //Its succending and preccending
+            //In terms of next and previous
+            public int noMhs;
+            public string name;
+            //point to the succending node
+            public Node next;
+            //point to the preceending node
+            public Node prev;
         }
-        
-
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
