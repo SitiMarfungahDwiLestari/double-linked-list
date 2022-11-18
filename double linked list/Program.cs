87c3e82cb04d204fd810486ba1329eed612b29c4
Siti Marfungah Dwi Lestari <siti.marfungah.ft21@mail.umy.ac.id>
2022-11-18 16:47:42 +0700

    Add project files.

---
+++ double linked list/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace double_linked_list
+{
+    class Program
+    {
+        class Node
+        {
+            //Node class 
+        }
+        
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
