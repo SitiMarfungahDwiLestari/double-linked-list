bb7983b9a52bf155579a2100c3efecb7133d2ea4
Siti Marfungah Dwi Lestari <siti.marfungah.ft21@mail.umy.ac.id>
2022-11-18 17:49:34 +0700

    add if the node is to be inserted at the end of the list

--- double linked list/Program.cs
+++ double linked list/Program.cs
@@ -65,6 +65,16 @@ namespace double_linked_list
                 be inserted*/
                 newNode.next = current;
                 newNode.prev = previous;
+
+                //if the node is to be inserted at the end of the list
+                if (current == null)
+                {
+                    newNode.next = null;
+                    previous.next = newNode;
+                    return;
+                }
+                current.prev = newNode;
+                previous.next = newNode;
             }
         }
 
